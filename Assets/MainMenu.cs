258df111a6acd6def19b90e5196fa5b61700320f
Bruce <tsz.lu.17@ucl.ac.uk>
2018-11-30 22:30:21 +0800

    UI MENU INIT

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void QuickStart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CustomStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame init");
+        Application.Quit();
+    }
+
+}
