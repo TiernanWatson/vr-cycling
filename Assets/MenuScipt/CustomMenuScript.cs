a8a91c67c8cc7b1f9c9e3e363cf020acb819a272
Bruce <tsz.lu.17@ucl.ac.uk>
2019-01-10 00:47:17 +0800

    starting menu more features

---
+++ Assets/MenuScipt/CustomMenuScript.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CustomMenuScript : MonoBehaviour {
+
+    bool selectByTime = true;
+    bool selectByDistance = false;
+    public Button targetButton;
+    public InputField primaryField;
+    public InputField secondaryField;
+    public Dropdown weatherDropDown;
+    public Dropdown terrainDropDown;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ResetVariable()
+    {
+        selectByTime = true;
+        selectByDistance = false;
+        targetButton.GetComponentInChildren<Text>().text = "Target By Time";
+        primaryField.placeholder.GetComponent<Text>().text = "Hour(s)";
+        secondaryField.placeholder.GetComponent<Text>().text = "Minute(s)";
+        weatherDropDown.value = 0;
+        terrainDropDown.value = 0;
+    }
+
+    private void ToTargetByTime()
+    {
+        this.selectByTime = true;
+        this.selectByDistance = false;
+        targetButton.GetComponentInChildren<Text>().text = "Target By Time";
+        primaryField.placeholder.GetComponent<Text>().text = "Hour(s)";
+        secondaryField.placeholder.GetComponent<Text>().text = "Minute(s)";
+        Debug.Log("switch from target by distance to target by time");
+    }
+
+    private void ToTargetByDistance()
+    {
+        this.selectByTime = false;
+        this.selectByDistance = true;
+        targetButton.GetComponentInChildren<Text>().text = "Target By Distance";
+        primaryField.placeholder.GetComponent<Text>().text = "km";
+        secondaryField.placeholder.GetComponent<Text>().text = "m";
+        Debug.Log("switch from target by time to target by distance");
+    }
+
+    public void SwitchTargetButtonPressed()
+    {
+        if (selectByTime)
+        {
+            ToTargetByDistance();
+        }else
+        {
+            ToTargetByTime();
+        }
+    }
+
+    public void StartButtonPressed()
+    {
+        Debug.Log("CustomStart init");
+        if (selectByDistance)
+        {
+            // return km and m
+        }
+        else
+        {
+            // return hour and minute
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
---
+++ Assets/MenuScipt/StartingMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StartingMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void QuickStart()
+    {
+        Debug.Log("QuickStart init");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame init");
+        Application.Quit();
+    }
+}
